5ca3a5be1af921feb69cfaf2eaa5e01152cc8aa9
Navpreet Singh <navpreetg477@gmail.com>
2025-10-14 22:25:29 -0500

    adding express.ts.

---
+++ src/api/v1/types/express.ts
@@ -0,0 +1,12 @@
+import 'express';
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: {
+      uid: string;
+      email?: string;
+      role?: string;
+      [k: string]: any;
+    };
+  }
+}
